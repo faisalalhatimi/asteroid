28636dfddc587160503dde37a32f012d34351955
FaisalAlhatimi <alhatimifaisaldev@gmail.com>
2026-08-22 17:04:27 +0300

    Add player class

---
+++ player.py
@@ -0,0 +1,25 @@
+from typing import override
+
+from circleshape import CircleShape
+from constants import LINE_WIDTH, PLAYER_RADIUS
+import pygame
+
+
+class Player(CircleShape):
+    rotation = 0
+
+    def __init__(self, x: float, y: float) -> None:
+        super().__init__(x, y, PLAYER_RADIUS)
+
+
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    @override
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
