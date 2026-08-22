5685220524ec824a8a0c82e56f09a795dd1f647f
FaisalAlhatimi <alhatimifaisaldev@gmail.com>
2026-08-22 17:05:04 +0300

    Add player object to gameloop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 # pygame setup
@@ -9,6 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
 
     while True:
         log_state()
@@ -20,6 +25,7 @@ def main():
         dt = clock.tick(60) / 1000
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
